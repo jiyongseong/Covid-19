8b9047bf0dd2d631b9a5c47444c83e6113a2c340
Ji Yong Seong <jiyongseong@hotmail.com>
2020-08-24 11:20:43 +0900

    initial commit of Covid-19 stats app

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Covid_19
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox_Country = new System.Windows.Forms.ComboBox();
+            this.chart_Covid19 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView_Covid19 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Covid19)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Covid19)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox_Country
+            // 
+            this.comboBox_Country.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox_Country.FormattingEnabled = true;
+            this.comboBox_Country.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox_Country.Items.AddRange(new object[] {
+            ""});
+            this.comboBox_Country.Location = new System.Drawing.Point(91, 11);
+            this.comboBox_Country.Name = "comboBox_Country";
+            this.comboBox_Country.Size = new System.Drawing.Size(262, 22);
+            this.comboBox_Country.Sorted = true;
+            this.comboBox_Country.TabIndex = 0;
+            this.comboBox_Country.SelectionChangeCommitted += new System.EventHandler(this.comboBox_Country_SelectionChangeCommitted);
+            // 
+            // chart_Covid19
+            // 
+            this.chart_Covid19.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "Covid-19Area";
+            this.chart_Covid19.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart_Covid19.Legends.Add(legend1);
+            this.chart_Covid19.Location = new System.Drawing.Point(12, 44);
+            this.chart_Covid19.Name = "chart_Covid19";
+            this.chart_Covid19.Size = new System.Drawing.Size(932, 677);
+            this.chart_Covid19.TabIndex = 1;
+            this.chart_Covid19.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Country";
+            // 
+            // dataGridView_Covid19
+            // 
+            this.dataGridView_Covid19.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView_Covid19.ColumnHeadersHeight = 34;
+            this.dataGridView_Covid19.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView_Covid19.GridColor = System.Drawing.SystemColors.AppWorkspace;
+            this.dataGridView_Covid19.Location = new System.Drawing.Point(950, 44);
+            this.dataGridView_Covid19.Name = "dataGridView_Covid19";
+            this.dataGridView_Covid19.ReadOnly = true;
+            this.dataGridView_Covid19.RowHeadersVisible = false;
+            this.dataGridView_Covid19.RowHeadersWidth = 62;
+            this.dataGridView_Covid19.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView_Covid19.RowTemplate.Height = 28;
+            this.dataGridView_Covid19.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView_Covid19.Size = new System.Drawing.Size(526, 677);
+            this.dataGridView_Covid19.TabIndex = 3;            
+            //
+            // Form
+            //
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1500, 768); 
+            this.Controls.Add(this.dataGridView_Covid19);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart_Covid19);
+            this.Controls.Add(this.comboBox_Country);
+            this.Font = new System.Drawing.Font("Cascadia Code", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Covid-19 Status by Country";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Covid19)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Covid19)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+        //
+        // Controls
+        //
+        private System.Windows.Forms.ComboBox comboBox_Country;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_Covid19;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView_Covid19;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Covid_19
+{
+    public partial class Form1 : Form
+    {
+        private class CountryCode
+        {
+            public string Country { get; set; }
+            public string Slug { get; set; }
+            public string ISO2 { get; set; }
+        }
+
+        private class Covid19
+        {
+            public DateTime Date { get; set; }
+            public int Confirmed { get; set; }
+            public int Deaths { get; set; }
+            public int Recovered { get; set; }
+            public int Active { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listCountry();
+
+            chart_Covid19.Series.Clear();
+            chart_Covid19.Series.Add("Confirmed").YValueMembers = "Confirmed";
+            chart_Covid19.Series.Add("Deaths").YValueMembers = "Deaths";
+            chart_Covid19.Series.Add("Recovered").YValueMembers = "Recovered";
+            chart_Covid19.Series.Add("Active").YValueMembers = "Active";
+
+            chart_Covid19.ChartAreas["Covid-19Area"].AxisX.LabelStyle.Font = new Font("Cascadia Code", 8F, System.Drawing.FontStyle.Regular);
+            chart_Covid19.ChartAreas["Covid-19Area"].AxisY.LabelStyle.Font = new Font("Cascadia Code", 8F, System.Drawing.FontStyle.Regular);
+            chart_Covid19.Legends[0].Font = new Font("Cascadia Code", 8F, System.Drawing.FontStyle.Regular);
+
+            chart_Covid19.ChartAreas["Covid-19Area"].AxisY.LabelStyle.Format = "#,###";            
+        }
+        private void listCountry()
+        {
+            string baseAddress = "https://api.covid19api.com/countries";
+            var httpClient = new HttpClient() { BaseAddress = new Uri(baseAddress) };
+            List<CountryCode> countries = Task.Run(() => httpClient.GetFromJsonAsync<List<CountryCode>>("")).Result;
+            countries.Add(new CountryCode { Country = "", Slug = "", ISO2 = "" });
+
+            comboBox_Country.DataSource = countries;
+            comboBox_Country.ValueMember = "Slug";
+            comboBox_Country.DisplayMember = "Country";
+
+            comboBox_Country.SelectedIndex = 0;
+
+        }
+        private void listChart(string slug)
+        {
+            string baseAddress = String.Format("https://api.covid19api.com/total/country/{0}", slug);
+            var httpClient = new HttpClient() { BaseAddress = new Uri(baseAddress) };
+            List<Covid19> allStatus = Task.Run(() => httpClient.GetFromJsonAsync<List<Covid19>>("")).Result;
+
+            chart_Covid19.DataSource = allStatus;
+
+            foreach (Series series in chart_Covid19.Series)
+            {
+                series.ChartType = SeriesChartType.Line;
+                series.YValueType = ChartValueType.Int32;
+                
+                series.XValueMember = "Date";
+                series.XValueType = ChartValueType.DateTime;
+            }
+            
+            chart_Covid19.DataBind();
+
+            dataGridView_Covid19.DataSource = new BindingSource(allStatus, null);
+
+            DataGridViewCellStyle numberCellStye = new DataGridViewCellStyle();
+            numberCellStye.Format = "#,##0";
+            numberCellStye.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            for (int i = 1; i <= 4; i++)
+            {
+                dataGridView_Covid19.Columns[i].DefaultCellStyle = numberCellStye;
+            }
+        }
+
+        private void comboBox_Country_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            string slug = ((CountryCode)comboBox_Country.SelectedItem).Slug.ToString();
+            //string fromDate = "2020-01-01T00:00:00Z";
+            //string toDate = String.Format("{0}T00:00:00Z", DateTime.UtcNow.ToString("yyyy-MM-dd"));
+            Form1.ActiveForm.Text = String.Format("Covid-19 Status by Country - {0}", ((CountryCode)comboBox_Country.SelectedItem).Country.ToString());
+            listChart(slug);
+        }        
+    }
+}
